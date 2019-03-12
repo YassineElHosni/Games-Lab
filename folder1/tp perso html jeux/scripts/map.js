70dfa6bd45370d221c049f201cbff72c1356299d
YassineH <hosni.yassine.yh@gmail.com>
2019-03-12 22:08:00 +0100

    replaced .bmp by .png

--- folder1/tp perso html jeux/scripts/map.js
+++ folder1/tp perso html jeux/scripts/map.js
@@ -2,8 +2,8 @@
 var mapImg = [];
 function mapImgLoad(){
   
-	mapImg[0] = loadImage("assets/blocks/block.bmp");
-	mapImg[1] = loadImage("assets/blocks/softBlock.bmp");
-	mapImg[2] = loadImage("assets/blocks/wall.bmp");
+	mapImg[0] = loadImage("assets/blocks/block.png");
+	mapImg[1] = loadImage("assets/blocks/softBlock.png");
+	mapImg[2] = loadImage("assets/blocks/wall.png");
 	
 }

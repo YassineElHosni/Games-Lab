1ed5e91efe3e732aeba1bf1f69769b4329b42108
YassineH <hosni.yassine.yh@gmail.com>
2019-03-12 22:29:13 +0100

    fixed link name error
    softBlock to SoftBlock, a weird unexpected commit, one of those you make just bc you can :innocent:

--- folder1/tp perso html jeux/scripts/map.js
+++ folder1/tp perso html jeux/scripts/map.js
@@ -3,7 +3,7 @@ var mapImg = [];
 function mapImgLoad(){
   
 	mapImg[0] = loadImage("assets/blocks/block.png");
-	mapImg[1] = loadImage("assets/blocks/softBlock.png");
+	mapImg[1] = loadImage("assets/blocks/SoftBlock.png");
 	mapImg[2] = loadImage("assets/blocks/wall.png");
 	
 }
